f81e319628929dbe0b14d08c95d2d61e5bb1839a
BJWry <66494821+johnwry@users.noreply.github.com>
2026-05-11 11:26:36 -0400

    update convert_pdf

--- CONVERT_PDF 2/filters/codeblock.lua
+++
@@ -1,6 +0,0 @@
-function CodeBlock(el)
-  return pandoc.RawBlock(
-    "latex",
-    "\\begin{rootsblock}\n" .. el.text .. "\n\\end{rootsblock}"
-  )
-end
--- CONVERT_PDF 2/filters/h4-to-hfour.lua
+++
@@ -1,7 +0,0 @@
-function Header(el)
-  -- Only convert H4
-  if el.level == 4 then
-    local text = pandoc.utils.stringify(el.content)
-    return pandoc.RawBlock("latex", "\\hfour{" .. text .. "}")
-  end
-end
