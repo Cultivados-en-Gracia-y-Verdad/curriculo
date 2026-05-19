8478cf5099fe1982b42c4f980427cc02a1582818
BJWry <66494821+johnwry@users.noreply.github.com>
2026-05-19 15:16:15 -0400

    update publish-map

--- scripts/publish-course.sh
+++ scripts/publish-course.sh
@@ -24,6 +24,9 @@ TARGET_IMAGES=$(echo "$LINE" | awk -F'\t' '{print $5}')
 SOURCE_QUIZZES=$(echo "$LINE" | awk -F'\t' '{print $6}')
 TARGET_QUIZZES=$(echo "$LINE" | awk -F'\t' '{print $7}')
 
+SOURCE_MANIFEST=$(echo "$LINE" | awk -F'\t' '{print $8}')
+TARGET_MANIFEST=$(echo "$LINE" | awk -F'\t' '{print $9}')
+
 echo "Publishing $COURSE"
 
 git fetch origin
@@ -36,25 +39,28 @@ git pull origin main
 mkdir -p "$(dirname "$TARGET_MD")"
 mkdir -p "$TARGET_IMAGES"
 mkdir -p "$TARGET_QUIZZES"
+mkdir -p "$(dirname "$TARGET_MANIFEST")"
 
 echo "Publishing markdown..."
 git show "origin/en-borrador:$SOURCE_MD" > "$TARGET_MD"
 
 echo "Publishing images..."
 git checkout origin/en-borrador -- "$SOURCE_IMAGES"
-
 rm -rf "$TARGET_IMAGES"
 mv "$SOURCE_IMAGES" "$TARGET_IMAGES"
 
 echo "Publishing quizzes..."
 git checkout origin/en-borrador -- "$SOURCE_QUIZZES"
-
 rm -rf "$TARGET_QUIZZES"
 mv "$SOURCE_QUIZZES" "$TARGET_QUIZZES"
+
+echo "Publishing manifest..."
+git show "origin/en-borrador:$SOURCE_MANIFEST" > "$TARGET_MANIFEST"
 
 git add "$TARGET_MD"
 git add "$TARGET_IMAGES"
 git add "$TARGET_QUIZZES"
+git add "$TARGET_MANIFEST"
 
 if git diff --cached --quiet; then
   echo "No changes to publish."
