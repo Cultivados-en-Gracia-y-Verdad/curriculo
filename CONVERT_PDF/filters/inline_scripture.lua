a73589063c6f9e4f4ad4fa836736a61b1c2e813e
BJWry <66494821+johnwry@users.noreply.github.com>
2026-05-19 14:56:00 -0400

    cleanup main

--- CONVERT_PDF/filters/inline_scripture.lua
+++
@@ -1,7 +0,0 @@
-function Emph(el)
-  if #el.c == 1 and el.c[1].t == "Str" then
-    local text = el.c[1].text
-    return pandoc.RawInline("latex", "\\scripture{«" .. text .. "»}")
-  end
-  return el
-end
--- CONVERT_PDF/filters/toc.lua
+++
@@ -1,9 +0,0 @@
-function Div(el)
-  if el.classes:includes("toc") then
-    return pandoc.RawBlock("latex", [[
-\setcounter{tocdepth}{1}
-\tableofcontents
-\clearpage
-]])
-  end
-end
